49c385024653d0d158b86dd49d11e37ae41183b2
vinicius webster <viniciuswebster99@gmail.com>
2023-07-02 11:36:50 -0300

    ajustes responsivos

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -3,7 +3,8 @@ import './card.css';
 const Card = (props) => {
     return(
         <div className='card' style={{
-            backgroundImage: `url(${props.image})`
+            backgroundImage: `url(${props.image})`,
+            backgroundSize: 'cover'
         }}>
             <h4>{props.title}</h4>
         </div>
