23c14333bd06411f525c97ca5f8db3f351d0f1b9
vinicius webster <viniciuswebster99@gmail.com>
2023-07-03 21:10:55 -0300

    ajustes de responsividade

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -11,9 +11,19 @@ const Banner = (props) => {
         "Support"
     ]
 
+    function abrirMenuMobile() {
+        const menuMobile = document.querySelector('.nav-container');
+
+        if (menuMobile.style.display === "none"){
+            menuMobile.style.display = "flex"
+        } else {
+            menuMobile.style.display = "none"
+        }
+    }
+
     return(
         <section className='banner'>
-            <Header nav={navItens}></Header>
+            <Header nav={navItens} closeMenu={abrirMenuMobile}></Header>
             <div className='container title'>
                 <h1>{props.text}</h1>
             </div>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,9 +8,14 @@ const Header = (props) => {
             </a>
             <nav>
                 <ul className='nav-container'>
+                <div className='menuMobile'>
+                    <img src='images/logo.svg' alt='logo loopstudios'/>
+                    <img className='icon-close' src='images/icon-close.svg' alt='logo loopstudios' onClick={props.closeMenu} />
+                </div>
                     {props.nav.map( item =><a href='git.com'><li>{item}</li> </a>)}
                 </ul>
             </nav>
+            <img className='icon-hamburger' src='images/icon-hamburger.svg' alt='Menu mobile' onClick={props.closeMenu}/>
         </header>
     )
 }
